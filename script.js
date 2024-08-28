411c7a9d810791e3f185afee2654a4b296de2812
saloni11042 <saloni11042@gmail.com>
2024-08-28 13:39:58 +0530

    best-seller-row updated

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
